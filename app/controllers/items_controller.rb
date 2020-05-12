8e463944fda2b2536ce013c53907b98abe000415
masanoriabe <masanori32@hotmail.com>
2020-05-12 11:36:11 +0900

    DBより最新の商品を3点を取得表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.order(updated_at: "DESC").first(3)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).premit(:name, :price, :explanation, :condition, :shipping_fee, :shipping_day)
+  end
+  
 end
