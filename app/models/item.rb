add18bb3c84faa15826c6ba575275a0465975bbe
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-13 15:56:51 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   validates :name, :explanation, :shipping_fee, :shipping_day, presence: true
-  validates :price, numericality: { only_integer: true, greater_than: 0, less_than: 9999999 }
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9999999 }
 
   has_many :images
 
