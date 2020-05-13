eab72d6e0f342f6906d03eb0b5c5d87a0f284e63
masanoriabe <masanori32@hotmail.com>
2020-05-13 12:51:01 +0900

    itemsテーブルにsold_dayカラム追加、ビューの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_025519) do
+ActiveRecord::Schema.define(version: 2020_05_13_034458) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "item_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_05_12_025519) do
     t.string "shipping_day", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "sold_day"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
