f93e8be1ff85d90b9af1d4b77faaf02b86aba600
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-13 17:23:52 +0900

    画像のみで保存不可能に

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   validates :name, :explanation, :shipping_fee, :shipping_day, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9999999 }
+  validates_associated :images
+  validates :images, presence: true
 
   has_many :images
 
