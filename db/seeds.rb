f3288d5c3c234ba397d70695a22afb0da2f79d8d
masanoriabe <masanori32@hotmail.com>
2020-05-14 16:08:20 +0900

    seed.rbにitemsとimages初期投入値を設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,186 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create!(
+  [
+    {
+      name: 'product1',
+      price: '10000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01',
+      user_id: '1'
+    },
+    {
+      name: 'product2',
+      price: '20000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-01-10',
+      updated_at: '2020-01-10',
+      user_id: '1'
+    },
+    {
+      name: 'product3',
+      price: '30000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-01-20',
+      updated_at: '2020-01-20',
+      user_id: '1'
+    },
+    {
+      name: 'product4',
+      price: '40000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-01-30',
+      updated_at: '2020-01-30',
+      user_id: '1'
+    },
+    {
+      name: 'product5',
+      price: '50000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-02-01',
+      updated_at: '2020-02-01',
+      user_id: '1'
+    },
+    {
+      name: 'product6',
+      price: '60000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-02-10',
+      updated_at: '2020-02-10',
+      sold_day: '2020-5-1',
+      user_id: '1'
+    },
+    {
+      name: 'product7',
+      price: '70000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-02-20',
+      updated_at: '2020-02-20',
+      user_id: '1'
+    },
+    {
+      name: 'product8',
+      price: '80000',
+      explanation: '説明',
+      condition: '良い',
+      shipping_fee: '着払い',
+      shipping_day: '１〜２日',
+      created_at: '2020-03-01',
+      updated_at: '2020-03-01',
+      user_id: '1'
+    }
+  ]
+)
+
+Image.create!(
+  [
+    {
+      item_id: '1',
+      image: '/assets/a001.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '2',
+      image: '/assets/a002.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '3',
+      image: '/assets/a003.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '4',
+      image: '/assets/a004.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '5',
+      image: '/assets/a005.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '6',
+      image: '/assets/a006.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '6',
+      image: '/assets/a007.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '7',
+      image: '/assets/a008.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-02'
+    },
+    {
+      item_id: '7',
+      image: '/assets/a009.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-01'
+    },
+    {
+      item_id: '8',
+      image: '/assets/a010.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-02'
+    },
+    {
+      item_id: '8',
+      image: '/assets/a011.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-03'
+    },
+    {
+      item_id: '8',
+      image: '/assets/a012.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-04'
+    },
+    {
+      item_id: '8',
+      image: '/assets/a013.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-05'
+    },
+    {
+      item_id: '8',
+      image: '/assets/a014.png',
+      created_at: '2020-01-01',
+      updated_at: '2020-01-06'
+    }
+  ]
+)
