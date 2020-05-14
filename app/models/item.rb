01159ba3fdfb235e16982dee30bc6b5504fd0dbc
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-14 20:55:43 +0900

    商品画像実装
    ・gem jqueryを用いた
    ・複数枚投稿機能
    ・画像削除機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   validates_associated :images
   validates :images, presence: true
 
-  has_many :images
-
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 end
