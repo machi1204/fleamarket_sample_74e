c3e3b4add4864763c73bb082e07de642de88ba47
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-19 10:23:18 +0900

    render 後の不具合を修正
    ・商品画像が選択できなくなる。
    ・レイアウトが崩れる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,8 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      redirect_to new_item_path
+      @item.images.build
+      render :new
     end
   end
 
