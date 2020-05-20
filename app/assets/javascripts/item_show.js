cca9331bb9ddd2cc6ddd0b00089de94f4c82a24c
masanoriabe <masanori32@hotmail.com>
2020-05-20 21:55:06 +0900

    ビュー修正（turbolink削除）

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -1,7 +1,7 @@
 
-$(function () {
+$(function(){
   $(".item-box-image__thumbnail").first().addClass("active");
-  $('.item-box-image__thumbnail').mouseover(function () {
+  $(".item-box-image__thumbnail").mouseover(function () {
     image_url = $(this).attr("src");
     $(".item-box-image__main").attr("src", image_url).hide().fadeIn();
     $(".item-box-image__thumbnail").removeClass("active");
