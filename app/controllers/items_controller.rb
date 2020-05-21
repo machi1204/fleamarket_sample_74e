d1a99e293d5598b3c749206e82891813fd087134
masanoriabe <masanori32@hotmail.com>
2020-05-22 00:02:57 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :destroy]
 
   def index
-    @items = Item.where(sold_day: nil).includes(:images).order(updated_at: "DESC")
   end
 
   def new
