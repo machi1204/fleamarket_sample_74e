f913bd150f4edc5ec84f34655dabac9c7c932b99
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-21 13:40:38 +0900

    カテゴリーのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
 
   validates :name, :explanation, presence: {message: "を入力してください"}
-  validates :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: {message: "を選択してください"}
+  validates :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :category_id, presence: {message: "を選択してください"}
   validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9999999, message: "入力してください。半角で入力してください。" }
   
-  # validates_associated :images, presence: true
   validates :images, presence: {message: "を選択してください"}
   accepts_nested_attributes_for :images, allow_destroy: true
 
