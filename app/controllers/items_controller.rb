f645070a26147d56af62a2967cbfbfeefa17a594
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-05-23 11:07:55 +0900

    カテゴリー編集サイズを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,9 +49,42 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  
+
+    grandchild_category = @item.category
+    child_category = grandchild_category.parent
+    
+    # 親カテゴリを取得
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    # 子カテゴリを取得
+    @category_children_array = []
+    Category.where(ancestry: nil).each do |children|
+      @category_children_array << children
+    end
+
+    # 孫カテゴリを取得
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+    
+    #サイズカテゴリを取得
+    @item_size_array = []
+    @size = @item.item_size.siblings.each do |size|
+      @item_size_array << size
+    end
+    # binding.pry
+
+
+    
   end
 
   def update
+  
     imageLength = 0
     deleteImage = 0
     params[:item][:images_attributes].each do |p|
@@ -84,13 +117,12 @@ class ItemsController < ApplicationController
     @images = Image.where(item_id: params[:id])
   end
   
- 
-
   private
   def item_params
     params.require(:item).permit(:name, :category_id, :item_size_id, :price, :brand, :explanation, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
       images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
   end
+
   
   def set_item
     @item = Item.find(params[:id])
