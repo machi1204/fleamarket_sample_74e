c2e20b4b8db0f80fd5e02b561e9c9d217e7a4d70
machi1204 <machi1204@icloud.com>
2020-05-24 18:21:21 +0900

    送信フォームまわりのビュー崩れを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
-    # redirect_to item_path(params[:item_id])
     @item = Item.find(params[:item_id])
     respond_to do |format|
       format.html { redirect_to item_path(params[:item_id]) }
